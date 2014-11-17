3c21cba3a17b07933f947927faf49757cb14dbdb
treken <chupekevin@gmail.com>
2014-11-16 19:48:37 -0600

    TWRP: add needed configurations for TWRP recovery
    Change-Id: Iae0a9001602ba641df02981eb3d4f79f1b80bade

--- device/jflte/oct.mk
+++ device/jflte/oct.mk
@@ -8,4 +8,7 @@ $(call inherit-product, vendor/oct/config/nfc_enhanced.mk)
 # Inherit some common oct stuff.
 $(call inherit-product, vendor/oct/config/common_phone.mk)
 
+# Build TWRP recovery
+include $(call all-makefiles-under,vendor/oct/config/twrp)
+
 PRODUCT_NAME := oct_jflte
